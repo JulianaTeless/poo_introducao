ed4e6511bc8c0c0766340a29c284e1bca49e39c7
Juliana Teles <jtelessoares.jt@gmail.com>
2022-10-13 17:30:58 -0300

    Classe Carro criada

---
+++ Carro.java
@@ -0,0 +1,21 @@
+public class Carro {
+    private String cor;
+    private String placa;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+}
